80dc87452315bf04ba89103641bcb33553a2b3cb
simonShulhin <42201346+simonShulhin@users.noreply.github.com>
2022-05-28 12:36:39 +0300

    bugs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function calculate(str) {
-    strArray = str.split(" ");    
+    // const strArray = ... 
+    strArray = str.split(" ");
+    // let answer = ""  тоді не треба буде дублювати в switch answer = ""
+      
     let answer;    
     const firstNumber = strArray[0].length;
     const secondNumber = strArray[2].length;
@@ -9,18 +12,21 @@ function calculate(str) {
         break;
       case "-":
         answer = "";
+            // цикл `for` винеси в окрему функцію
         for( let i=0; i < firstNumber - secondNumber; i++) {
           answer = answer + ".";        
         }
         break;  
         case "*":
         answer = "";
+            // цикл `for` винеси в окрему функцію
         for( let i=0; i < firstNumber * secondNumber; i++) {
           answer = answer + ".";        
         }
         break;  
         case "//":
-        answer = "";        
+        answer = ""; 
+            // цикл `for` винеси в окрему функцію
         for( let i=0; i < Math.trunc(firstNumber / secondNumber); i++) {
           answer = answer + ".";        
         }
@@ -33,6 +39,7 @@ function calculate(str) {
 
   function grid(number) {
     if (number < 0) {
+        // не лишай консолів, ти маєш повертати якесь значення, консолі слугують лише як допоміжні методи, а ти тут нічого не повертаєш
       console.log(null);
       return
     }
@@ -47,8 +54,12 @@ function calculate(str) {
         if (num > 25) num = 0;
         string = string + ' ' + alphabet[num] ;
       }
+        
+      // те саме тут
       console.log(string);
     }       
+      // і тут
+      // ти маєш повернути масив масивів grid(3) --> [[a,b,c],[b,c,d],[c,d,e]]
     return
   }
   //grid(10);
@@ -56,11 +67,13 @@ function calculate(str) {
   function isValidParentheses(string) {
     let answer;
     let stringArray = [];    
-    for (let i = 0; i < string.length; i++) {      
+    for (let i = 0; i < string.length; i++) {
+        // використовуй === замість ==
       if (string[i] == ')') {
         if (stringArray.slice(-1) == '(') stringArray.pop(); else stringArray.push(string[i]);
       } else stringArray.push(string[i]);      
-    }    
+    }
+      // використовуй === замість ==
     if (stringArray.length == 0) answer = true; else answer = false;
     return answer;
 }
